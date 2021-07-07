bcca45b8623b336021480945c5dd1840df5a6ffe
Fahim <fahimalemroz@gmail.com>
2021-07-07 16:48:34 +0600

    search sidebar made visible

--- src/components/LocationSearch/LocationSearch.js
+++ src/components/LocationSearch/LocationSearch.js
@@ -1,22 +1,23 @@
-import { useState } from 'react';
-import LocationResult from './LocationResult';
+import { useState , useContext } from "react";
+import {AppContext} from '../../contexts/AppContext';
+import LocationResult from "./LocationResult";
 
 function LocationSearch() {
-//	const { AppData, dispatch } = useContext(AppContext);
-//
-//	const geoCoderURL = locationToLookFor =>
-//		`http://api.openweathermap.org/geo/1.0/direct?q=${locationToLookFor}&limit=${1}&appid=${
-//			AppData.apiKey
-//		}`;
-//
-	const [locationSearchTerm, setLocationSearchTerm] = useState('');
+	const { AppData, dispatch } = useContext(AppContext);
+
+	const geoCoderURL = (locationToLookFor) =>
+		`http://api.openweathermap.org/geo/1.0/direct?q=${locationToLookFor}&limit=${1}&appid=${
+			AppData.apiKey
+		}`;
+
+	const [locationSearchTerm, setLocationSearchTerm] = useState("");
 
 	return (
-		<section className="location-search bg-green-100">
+		<section className="location-search bg-green-400">
 			<input
 				type="text"
 				value={locationSearchTerm}
-				onChange={e => setLocationSearchTerm(e.target.value)}
+				onChange={(e) => setLocationSearchTerm(e.target.value)}
 				placeholder="Search Location"
 			/>
 
