be407faa15a14f0b860ca590315222298b4b593c
Fahim <fahimalemroz@gmail.com>
2021-07-08 08:22:03 +0600

    Continent name for searched location being rendered

--- src/components/LocationSearch/LocationResult.js
+++ src/components/LocationSearch/LocationResult.js
@@ -1,11 +1,26 @@
-function LocationResult({foundLocation}) {
+import continents from "../../continents.json";
+
+function LocationResult({ foundLocation }) {
+	
+	const locationContinent = (() => {
+		for (const continentName in continents) {
+			if (continents[continentName].includes(foundLocation.country)) return continentName;
+		}
+	})()
+
 	return (
 		<div className="location-result">
-			<h2 className="location-name">{foundLocation.name + ', ' + foundLocation.country}</h2>
+			<h2 className="location-name">
+				{foundLocation.name + ", " + foundLocation.country}
+			</h2>
 
-			<h4>({foundLocation.lat.toFixed(0) + ', ' + foundLocation.lon.toFixed(0)})</h4>
+			<h4>
+				(
+				{foundLocation.lat.toFixed(0) + ", " + foundLocation.lon.toFixed(0)}
+				)
+			</h4>
 
-			<h3>LOCATION_CONTINENT</h3>
+			<h3 className="capitalize">{locationContinent}</h3>
 		</div>
 	);
 }
