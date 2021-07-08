5432a935406f16040664dabb1b0c1931ce1a3dcf
Fahim <fahimalemroz@gmail.com>
2021-07-08 14:34:34 +0600

    Location 404 error handled

--- src/components/LocationSearch/LocationSearch.js
+++ src/components/LocationSearch/LocationSearch.js
@@ -13,10 +13,17 @@ function LocationSearch() {
 		}`;
 
 	const handleSearchTermChange = async (e) => {
+		// executing operations on enter press and truthy search term
 		if (e.target.value && e.keyCode === 13) {
 			try {
 				const requestResponse = await fetch(geoCoderURL(e.target.value));
 				const responseData = await requestResponse.json();
+
+				// alerting the user if searched location doesn't exist
+				if (responseData.cod === "404" || !responseData[0]) {
+					alert(`We couldn't find "${e.target.value}"`);
+					e.target.value = "";
+				}
 
 				// responseData being set to foundLocations only if data is properly received as an array as the server sends an object if location not found
 				if (Array.isArray(responseData)) {
@@ -25,11 +32,11 @@ function LocationSearch() {
 						...responseData,
 					]);
 				}
-			} catch (e) {
-				console.log(e);
+			} catch (err) {
+				console.log(err);
 			}
 		}
-
+		// setting the foundLocations array to be empty if user clears search term
 		if (e.keyCode === 8) setFoundLocations([]);
 	};
 
