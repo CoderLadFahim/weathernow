909b3ff0606f6f5beb02a2af5680eedbf9238c21
Fahim <fahimalemroz@gmail.com>
2021-07-09 18:09:59 +0600

    Added data cards

--- src/components/MainDataCard.js
+++ src/components/MainDataCard.js
@@ -1,11 +1,14 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCloud } from "@fortawesome/free-solid-svg-icons";
+import { faWind } from "@fortawesome/free-solid-svg-icons";
+import { faTint } from "@fortawesome/free-solid-svg-icons";
+import { faRoad } from "@fortawesome/free-solid-svg-icons";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
 import { useState, useContext } from "react";
 import { AppContext } from "../contexts/AppContext";
 
 function MainDataCard({ locationTimezone, mainData }) {
 	const {
-		AppData: { unitSytem },
+		AppData: { unitSystem },
 	} = useContext(AppContext);
 
 	return (
@@ -17,16 +20,56 @@ function MainDataCard({ locationTimezone, mainData }) {
 				<h1>{mainData && mainData.weather[0].description}</h1>
 				<h1 className="temperature">{mainData && mainData.temp}</h1>
 			</div>
-			
+
 			{mainData && (
 				<img
 					src={`http://openweathermap.org/img/wn/${mainData.weather[0].icon}@2x.png`}
-					alt=""
+					alt="weather icon"
 				/>
 			)}
 
-			<div className="data-card">
-				<FontAwesomeIcon icon={faCloud} className="data-icon" />
+			{/* Data Cards */}
+
+			<div className="data-cards">
+				<div className="data-card">
+					<FontAwesomeIcon icon={faWind} className="data-icon" />
+					<div className="data-info">
+						<h1 className="data">{mainData && mainData.wind_speed + `${unitSystem === 'metric' ? 'm/s' : 'mph'}`}</h1>
+						<h2 className="data-label">Wind Speed</h2>
+					</div>
+				</div>
+
+				{/* Humidity */}
+
+				<div className="data-card">
+					<FontAwesomeIcon icon={faTint} className="data-icon" />
+					<div className="data-info">
+						<h1 className="data">{mainData && mainData.humidity}%</h1>
+						<h2 className="data-label">Humidity</h2>
+					</div>
+				</div>
+
+				{/* Visibility */}
+				
+				<div className="data-card">
+					<FontAwesomeIcon icon={faRoad} className="data-icon" />
+					<div className="data-info">
+						<h1 className="data">{mainData && mainData.visibility + `${unitSystem === 'metric' ? 'm' : 'mi'}`}</h1>
+						<h2 className="data-label">Visibility</h2>
+					</div>
+				</div>
+
+				
+				{/* UVI */}
+				
+			
+				<div className="data-card">
+					<FontAwesomeIcon icon={faSun} className="data-icon" />
+					<div className="data-info">
+						<h1 className="data">{mainData && mainData.uvi}</h1>
+						<h2 className="data-label">UV Index</h2>
+					</div>
+				</div>
 			</div>
 
 			<pre className="current-location">
