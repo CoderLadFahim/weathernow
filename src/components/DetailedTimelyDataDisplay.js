faf43f762db17531419667d8fa67f597ed4f6f45
Fahim <fahimalemroz@gmail.com>
2021-07-16 19:37:32 +0600

    DetailedTimelyDataDisplay markup done, needs more functionality

--- src/components/DetailedTimelyDataDisplay.js
+++ src/components/DetailedTimelyDataDisplay.js
@@ -1,18 +1,28 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import moment from 'moment';
 import TimelyDataToggler from './TimelyDataToggler';
+import DataCardDisplay from './DataCardDisplay';
+
+import { useContext } from 'react';
+import { AppContext } from '../contexts/AppContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 function DetailedTimelyDataDisplay({ data, hideDataDisplay }) {
 	const [weather] = data.weather;
 	const iconCode = weather.icon;
+	const {
+		AppData: { timelyDataType, unitSystem },
+	} = useContext(AppContext);
 
 	return (
-		<div className="detailed-data-display border text-indigo-300">
+		<div className="detailed-data-display border bg-indigo-800">
 			<TimelyDataToggler />
+
 			<div className="details-card">
 				<div className="card-header">
-					<h2 className="day">{moment.unix(data.dt).format('dddd')}</h2>
+					{timelyDataType === 'hourly' && (
+						<h2 className="day">{moment.unix(data.dt).format('dddd')}</h2>
+					)}
 					<FontAwesomeIcon
 						className="cursor-pointer"
 						onClick={hideDataDisplay}
@@ -24,6 +34,26 @@ function DetailedTimelyDataDisplay({ data, hideDataDisplay }) {
 						src={`http://openweathermap.org/img/wn/${iconCode}@2x.png`}
 						alt="weather icon"
 					/>
+
+					<div className="temps">
+						{unitSystem === 'metric' ? (
+							<>
+								<p className="celsius">28°C</p>
+
+								<p className="fahrenheit">55°F</p>
+							</>
+						) : (
+							<>
+								<p className="fahrenheit">55°F</p>
+
+								<p className="celsius">28°C</p>
+							</>
+						)}
+					</div>
+
+					<h2 className="weather-desc">{weather.description}</h2>
+
+					<DataCardDisplay />
 				</div>
 			</div>
 		</div>
