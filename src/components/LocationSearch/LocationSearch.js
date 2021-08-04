6945a23d804706afba99f21fcf07045bd6e15442
Fahim <fahimalemroz@gmail.com>
2021-08-04 11:31:49 +0600

    Search sidebar designed for mobile

--- src/components/LocationSearch/LocationSearch.js
+++ src/components/LocationSearch/LocationSearch.js
@@ -1,12 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 import { useState, useContext } from 'react';
 import { AppContext } from '../../contexts/AppContext';
 import LocationResult from './LocationResult';
 
 function LocationSearch() {
-	const { AppData } = useContext(AppContext);
-
+	const { AppData, dispatch } = useContext(AppContext);
 	const [foundLocations, setFoundLocations] = useState([]);
-
 	const geoCoderURL = (locationToLookFor) =>
 		`http://api.openweathermap.org/geo/1.0/direct?q=${locationToLookFor}&limit=${50}&appid=${
 			AppData.apiKey
@@ -40,18 +41,29 @@ function LocationSearch() {
 		if (e.keyCode === 8) setFoundLocations([]);
 	};
 
+	const hideSearchMenu = () =>
+		dispatch({
+			type: 'SET_USER_SEARCHING_LOCATION',
+			payload: !AppData.userSearchingLocation,
+		});
+
 	return (
-		<section className="location-search w-1/4 bg-green-400 z-10 absolute top-0 bottom-0 right-0 pt-7 shadow text-center">
+		<section className="location-search w-4/5 bg-green-400 z-10 absolute top-0 bottom-0 right-0 pt-7 shadow text-center">
 			<div className="backdrop"></div>
 			<input
 				type="text"
-				className="text-center bg-gray-100 mb-5 text-gray-500 text-font-bold outline-none rounded-xl py-3 w-5/6"
+				className="text-center bg-gray-100 mb-5 text-gray-500 text-font-bold outline-none rounded-xl py-3 w-5/6 shadow"
 				onKeyUp={handleSearchTermChange}
 				placeholder="Search Location"
 			/>
 			{foundLocations.map((location, i) => (
 				<LocationResult key={i} foundLocation={location} />
 			))}
+			<FontAwesomeIcon
+				className="absolute text-green-200 cursor-pointer  bottom-4 right-5 transform scale-150 hover:text-white"
+				onClick={hideSearchMenu}
+				icon={faTimes}
+			/>
 		</section>
 	);
 }
