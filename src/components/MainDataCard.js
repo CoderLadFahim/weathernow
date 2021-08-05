42651f804f8b852452b2f5594b1cc1734b7bb6bf
Fahim <fahimalemroz@gmail.com>
2021-08-05 11:48:57 +0600

    Added designs to the top part of MainDataCard

--- src/components/MainDataCard.js
+++ src/components/MainDataCard.js
@@ -14,27 +14,38 @@ function MainDataCard({ locationTimezone, mainData }) {
 		mainData && moment.unix(mainData.dt).format('h:mm A, dddd');
 
 	return (
-		<section className="main-data-card container w-1/2 bg-gray-200 mt-3 text-gray-800 rounded-2xl shadow">
-			<div className="current-weather-data">
-				<h1 className="location-timezone">{locationTimezone}</h1>
-				<h2 className="time">{currentTime}</h2>
+		<section className="main-data-card container w-1/2 bg-gray-200 mt-3 text-gray-800 rounded-2xl shadow px-5 py-3">
+			<div className="flex">
+				<div className="current-weather-data">
+					<h1 className="location-timezone font-bold text-gray-600">
+						{locationTimezone}
+					</h1>
+					<h2 className="time text-blue-400 font-bold">{currentTime}</h2>
 
-				<h1>{weatherDescription}</h1>
-				<h1 className="temperature">
-					{temperature}
+					<h1 className="font-bold text-lg capitalize">
+						{weatherDescription}
+					</h1>
+					<h1 className="temperature num-font text-5xl font-bold relative">
+						<span
+							className={`text-${
+								unitSystem === 'metric' ? 'green' : 'blue'
+							}-400`}
+						>
+							{temperature.toFixed(0)}
+						</span>
 
-					<span className="temp-unit">
-						°{unitSystem === 'metric' ? 'C' : 'F'}
-					</span>
-				</h1>
+						<span className="temp-unit text-xs text-gray-600 absolute top-0">
+							°{unitSystem === 'metric' ? 'C' : 'F'}
+						</span>
+					</h1>
+				</div>
+				{mainData && (
+					<img
+						src={`http://openweathermap.org/img/wn/${mainData.weather[0].icon}@2x.png`}
+						alt="weather icon"
+					/>
+				)}
 			</div>
-			{mainData && (
-				<img
-					src={`http://openweathermap.org/img/wn/${mainData.weather[0].icon}@2x.png`}
-					alt="weather icon"
-				/>
-			)}
-
 			<DataCardsDisplay />
 		</section>
 	);
