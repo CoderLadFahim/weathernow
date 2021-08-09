4973be44b5e804c9506913e990490ec03cf9be6c
Fahim <fahimalemroz@gmail.com>
2021-08-09 12:37:56 +0600

    Designed the top part of MainDataCard

--- src/components/MainDataCard.js
+++ src/components/MainDataCard.js
@@ -14,20 +14,20 @@ function MainDataCard({ locationTimezone, mainData }) {
 		mainData && moment.unix(mainData.dt).format('h:mm A, dddd');
 
 	return (
-		<section className="main-data-card container flex flex-col justify-around w-1/2 h-1/2 bg-gray-200 text-gray-800 rounded-2xl shadow px-5 py-3 relative">
+		<section className="main-data-card container flex flex-col justify-around xs:justify-between  w-1/2 h-1/2 bg-gray-200 text-gray-800 rounded-2xl shadow px-5 py-3 relative">
 			<div className="flex mb-4">
 				<div className="current-weather-data">
-					<h1 className="location-timezone font-bold text-gray-600 relative text-xs">
+					<h1 className="location-timezone font-bold text-gray-600 relative text-xs xs:text-sm">
 						{locationTimezone}
 					</h1>
-					<h2 className="time text-blue-400 font-bold text-sm">
+					<h2 className="time text-blue-400 font-bold text-sm xs:text-base">
 						{currentTime}
 					</h2>
 
-					<h1 className="font-extrabold text-xl mb-1 capitalize">
+					<h1 className="font-extrabold text-xl xs:text-2xl mb-1 capitalize">
 						{weatherDescription}
 					</h1>
-					<h1 className="temperature num-font text-6xl font-bold relative">
+					<h1 className="temperature num-font text-6xl xs:text-7xl font-bold relative">
 						<span
 							className={`text-${
 								unitSystem === 'metric' ? 'green' : 'blue'
@@ -44,7 +44,7 @@ function MainDataCard({ locationTimezone, mainData }) {
 				{mainData && (
 					<img
 						src={`http://openweathermap.org/img/wn/${mainData.weather[0].icon}@2x.png`}
-						className="w-32 absolute right-0"
+						className="w-32 absolute xs:w-44 right-0"
 						alt="weather icon"
 					/>
 				)}
